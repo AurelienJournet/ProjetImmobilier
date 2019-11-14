697150781e1eee6f1f1ca78995ee48c33bfcf65d
aurélien journet <aureljournet@orange.fr>
2019-11-14 13:40:30 +0100

    Ajout User.java

--- src/main/java/com/fr/adaming/entity/User.java
+++ src/main/java/com/fr/adaming/entity/User.java
@@ -1,9 +1,13 @@
 package com.fr.adaming.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,5 +20,13 @@ public class User {
 	@Id
 	@GeneratedValue
 	private Long id;
+	
+	@Column(nullable = false,unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String fullName;
+	
+	private int telephone;
 
 }
